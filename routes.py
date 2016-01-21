46d19647a4d413fee44e56ea67fa7aa11c0f2bd2
root <derekkooyman@gmail.com>
2016-01-21 06:37:40 -0700

    Updated routes.py with basic routes

--- routes.py
+++ routes.py
@@ -1,2 +1,34 @@
 #This file will contain the routes, to perform CRUD operations on
 #All of the items in the catalog
+
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+app = Flask(__name__)
+
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Sub_Category, Item
+import login_auth
+
+#Connect to Database and create Database session
+APPLICATION_NAME = "Item Catalogue"
+
+engine = create_engine('sqlite:///catalogueitems.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/category/')
+def showCategories():
+    return "This is the main page"
+
+@app.route('/category/new/', methods=['GET', 'POST'])
+def newCategory():
+    return "This where new categories are added"
+
+
+
+if __name__ =='__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
