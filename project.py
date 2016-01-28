816a30921803e4697be6c10f939588f79fe94083
root <derekkooyman@gmail.com>
2016-01-28 07:50:11 -0700

    Added templates to routes

--- project.py
+++ project.py
@@ -27,6 +27,17 @@ categories
 def showCategories():
     category = session.query(Category).first()
     items = session.query(Item).filter_by(category_id = category.id)
+    return render_template('index.html', category = category, items=items)
+
+@app.route('/category/new/', methods=['GET', 'POST'])
+def newCategory():
+    return "This where new categories are added"
+
+@app.route('/category/<string:name>/edit/')
+def editCategory(name):
+    category = session.query(Category).filter_by(name = name).one()
+    id = category.id
+    items = session.query(Item).filter_by(category_id = category.id)
     output = ''
     for i in items:
         output += i.name
@@ -35,32 +46,41 @@ def showCategories():
         output += '</br>'
     return output
 
-@app.route('/category/new/', methods=['GET', 'POST'])
-def newCategory():
-    return "This where new categories are added"
+@app.route('/category/<string:name>/delete/')
+def deleteCategory(name):
+    category = session.query(Category).filter_by(name = name).one()
+    id = category.id
+    items = session.query(Item).filter_by(category_id = category.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+    return output
 
-@app.route('/category/category_id/edit')
-def editCategory():
-    return "This is where categories are edited"
+@app.route('/category/<string:name>/new/', methods=['GET', 'POST'])
+def newCategoryItem(name):
+    if request.method == 'POST':
+        newItem = Item(name = request.form['name'])
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('showCategories'))
 
-@app.route('/category/category_id/delete')
-def deleteCategory():
-    return "This is where categories are deleted"
-
-@app.route('/category/category_id/item')
-def showItems():
-    return "This is where the items are displayed"
+    else:
+        return render_template('newCategoryItem.html', name = name)
+    return "page to create a new item Task 1 complete"
 
 @app.route('/category/category_id/item/new')
 def createItem():
     return "This is where items are created"
 
-@app.route('/category/category_id/item/item_id/edit')
-def editItem():
+@app.route('/category/<string:name>/<string:item_name>/edit/')
+def editCategoryItem(name, item_name):
     return "This is where items are edited"
 
-@app.route('/category/category_id/item/item_id/delete')
-def deleteItem():
+@app.route('/category/<string:name>/<string:item_name>/delete/')
+def deleteCategoryItem(name, item_name):
     return "This is where items are deleted"
 
 
