98ad55de10a7f2b080debe30b95116e97791e307
root <derekkooyman@gmail.com>
2016-03-04 14:03:57 -0700

    Added the gconnect function for user authentication, temporarily ignoring the client_secrets.json to keep the client secret off github

--- project.py
+++ project.py
@@ -17,7 +17,10 @@ import random, string
 from flask import make_response
 from flask import session as login_session
 import requests
-#from functions import getCategories, getCategory
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+
 
 #Connect to Database and create Database session
 APPLICATION_NAME = "Item Catalogue"
@@ -181,8 +184,87 @@ def categoryItemsJSON(name):
 @app.route('/login')
 def showLogin():
     state = tokenMaker()
-    #login_session['state'] = state
+    login_session['state'] = state
     return render_template('login.html')
+
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        print("Error1")
+        return response
+    code = request.data
+    try:
+    # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        print("Error2")
+        return response
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+            % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+
+        response.headers['Content-Type'] = 'application/json'
+        #Verify that the access code is used for the intended user
+        gplus_id = credentials.id_token['sub']
+        if result['user_id'] != gplus_id:
+            response = make_response(
+                json.dumps("Token's user ID doesn't match given user ID."),
+            401)
+        response.headers['Content-Type'] = 'application/json'
+        print("Error3")
+        return response
+    #Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's!"),
+        401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    #Check to see if the user is already logged in
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected'),
+        200)
+        response.headers['Content-Type'] = 'application/json'
+    #Store the access token in the session for later use.
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    #Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt':'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' "style = width: 300px; height: 300px; border-radius: 150px; -webkit-border-radius: 150px; -moz-border-radius: 150px;">'
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 
 #The following are routes for basic error handling
 @app.errorhandler(404)
@@ -190,5 +272,6 @@ def page_not_found(e):
     return render_template('404.html')
 
 if __name__ =='__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
